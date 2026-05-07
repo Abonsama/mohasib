33ca16292d75b6239cbc94c6ad4887d98c4ae390
Mohamed Adil <aboon20018@gmail.com>
2026-05-07 05:59:53 +0300

    add: made form button and basic folder structure

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,11 @@
 'use client'
-
+import Link from "next/link"
+import Nav from "./components/nav"
 
 export default function Home() {
+  return(
+    <section>
+      <Link href={"/formPage"}>add new transaction</Link>
+    </section>
+  );
 }
